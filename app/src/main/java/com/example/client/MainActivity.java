4c415d6038f562e14875b8f1ced91122c7366ae1
trubashatal8 <trubashatal8@mail.ru>
2020-04-28 23:47:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/client/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.client;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+
+public class MainActivity extends AppCompatActivity {
+
+    Switch myswitch;
+    Button buttonLog;
+    TextView password,login,buttonReg;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.darktheme);
+        }
+        else setTheme(R.style.AppTheme);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        myswitch=(Switch) findViewById(R.id.switch1);
+
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            myswitch.setChecked(true);
+        }
+
+        myswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    Intent i = getBaseContext().getPackageManager().getLaunchIntentForPackage(getBaseContext().getPackageName());
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(i);
+                    finish();
+                }
+                else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    Intent i = getBaseContext().getPackageManager().getLaunchIntentForPackage(getBaseContext().getPackageName());
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        });
+
+
+
+        buttonLog=findViewById(R.id.buttonlog);
+        buttonReg=findViewById(R.id.textbuttonreg);
+
+        password=findViewById(R.id.password);
+        login=findViewById(R.id.login);
+
+        buttonLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String log,pass;
+                log=login.getText().toString();
+                pass=password.getText().toString();
+                if(pass==""){
+                    Toast.makeText(MainActivity.this,"Bad pass",Toast.LENGTH_SHORT);
+                }else
+                if(log==""){
+                    Toast.makeText(MainActivity.this,"Bad login",Toast.LENGTH_SHORT);
+                }else {
+                    //Авторизация
+                    Toast.makeText(MainActivity.this, "Nice dick", Toast.LENGTH_SHORT);
+                }
+
+            }
+        });
+
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,Registration.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
